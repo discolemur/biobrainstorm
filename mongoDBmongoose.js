5f86c114b23e84581d37d4d35fccd34c9aa166c8
rphillary <rphillary@gmail.com>
2014-11-22 16:16:26 -0700

    Update mongoDBmongoose.js
    Added functionality: save and find. I will begin making forming the needed handlers rather than having it be a forced.

--- mongoDBmongoose.js
+++ mongoDBmongoose.js
@@ -21,7 +21,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   // yay!
-  console.log("Connected Successfully")
+  console.log("Connected to usersDB Successfully")
 });
 
 var Schema = mongoose.Schema;
@@ -36,3 +36,39 @@ var Users = mongoose.model('Users',userSchema)
 
 var defaultUser = new Users({userName: 'Test1', passWord: 'Test1', emailAddress:'Test1@awesome.com'})
 console.log(defaultUser)
+
+defaultUser.save(function (err, data) {
+if (err) console.log(err);
+else console.log('Saved ', data );
+});
+
+//console.log(db.collection.getIndexes());
+
+Users.find().distinct('_id', function(error, ids) {
+    // ids is an array of all ObjectIds
+    console.log("Found the following IDs")
+    console.log(ids)
+});
+
+mongoose.connect('mongodb://localhost/Users/ryanhillary/Desktop/nodejs/questionsDB');
+var dbQuestions = mongoose.connection;
+dbQuestions.on('error', console.error.bind(console, 'connection error:'));
+dbQuestions.once('open', function callback () {
+  // yay!
+  console.log("Connected to questionsDB Successfully")
+});
+
+
+var questionsSchema = new Schema({
+  //messageId = ObjectId, //built in to mongoDB, its one freaking long ID
+  userName:  String, 
+  tagTopics: [String], // possible method to find search results
+  title: String, 
+  //messageType:   String,
+  messageBody: String,
+  childrenIds: Array,
+  voteNum: Number,
+});
+
+var Questions = mongoose.model('Questions',questionsSchema)
+
