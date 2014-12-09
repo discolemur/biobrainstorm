5ccf36acae5ae34515e09090baed738b3d54e803
rphillary <rphillary@gmail.com>
2014-12-09 00:14:56 -0700

    Update router.js

--- router.js
+++ router.js
@@ -26,8 +26,23 @@ router.post('/signupjson', function(req,res){
 	res.send(req.body);
 });
 
+
+
 router.get('/home', function(req, res) {
 	res.render('home', { title: 'Biobrainstorm : a bioinformatics community' });
+});
+//added RH
+router.get('/search', function(req, res) {
+	res.render('search', { title: 'Search' });
+		var handler = require('./search2');
+
+	var handle_search = function(jsonBroOut) {
+		//console.log("RETURNED!: ",jsonBroOut);
+		//_callback(jsonBroOut);
+		res.send(jsonBroOut);
+	};
+
+handler.search(req.body, handle_search);
 });
 
 router.get('/signin', function(req, res) {
@@ -39,7 +54,7 @@ router.get('/signin', function(req, res) {
 		//_callback(jsonBroOut);
 		res.send(jsonBroOut);
 	}
-	handler.signin(jsonBroIn, handle_signin);
+	handler.signin(req.body, handle_signin);
 	
 });
 
@@ -52,7 +67,36 @@ router.get('/signup', function(req, res) {
 		//_callback(jsonBroOut);
 		res.send(jsonBroOut);
 	}
-	hanlder.signup(body.req, handle_signup)
+	hanlder.signup(req.body, handle_signup)
+});
+
+router.get('/post_question', function(req, res) {
+	res.render('post_question', { title: 'Post Your Question' });
+});
+//added RH
+router.get('/post_response', function(req, res) {
+	res.render('post_response', { title: 'Post Your Response' });
+});
+//added RH
+router.get('/voting', function(req, res) {
+	res.render('voting', { title: 'Vote' });
+});
+//added RH
+router.get('/get_one_question', function(req, res) {
+	res.render('/get_one_question', { title: 'View Question' });
+	
+});
+//added RH to be implemented later, works on back end code
+router.get('/get_one_question', function(req, res) {
+	res.render('get_one_question', { title: 'View Response' });
+});
+//added RH
+router.get('/delete_question', function(req, res) {
+	res.render('delete_question', { title: 'Delete Question' });
+});
+//added RH
+router.get('/delete_response', function(req, res) {
+	res.render('delete_response', { title: 'Delete Response' });
 });
 
 router.get('/about', function(req, res) {
@@ -63,9 +107,7 @@ router.get('/contact', function(req, res) {
 	res.render('contact', { title: 'Contact' });
 });
 
-router.get('/post_question', function(req, res) {
-	res.render('post_question', { title: 'Post Your Question' });
-});
+
 
 router.get('/view_unanswered', function(req, res) {
 	res.render('view_unanswered', { title: 'All Questions' });
