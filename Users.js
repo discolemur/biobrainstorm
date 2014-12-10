5a592f56d90cb7cc17ab187aaf0f1f8edd48d3b1
rphillary <rphillary@gmail.com>
2014-12-09 21:10:00 -0700

    Moving the User Schema to its own file

---
+++ Users.js
@@ -0,0 +1,10 @@
+  var mongoose = require('mongoose');
+  var Schema = mongoose.Schema;
+
+  var userSchema = new Schema({
+    userName:  String,
+    passWord: String,
+    emailAddress:   String,
+    
+  });
+module.exports = mongoose.model('Users', userSchema); 
