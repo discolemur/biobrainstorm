a7610f08c1981a3c90d7111d847a7a45a8c74c9b
rphillary <rphillary@gmail.com>
2014-12-09 22:20:18 -0700

    Create voting2.js

---
+++ voting2.js
@@ -0,0 +1,60 @@
+  
+  var voting  = function callback (jsonBroIn, _callback) { 
+
+  console.log("voting")
+
+  //var jsonCast = JSON.parse(jsonBroIn);
+  //var id = jsonCast.id;
+  //var voteType = jsonCast.voteType;
+
+  var id = jsonBroIn.id;
+  var voteType = jsonBroIn.voteType;
+
+
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost/Users/ryanhillary/Desktop/nodejs/userDB');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function callback () {
+    // yay!
+    console.log("Connected to usersDB Successfully")
+  });
+
+  var messageDB = require('./Messages');
+
+  messageDB.findOne({_id: id}, function(err, message){
+  if (err) { 
+    var rejected = {'status':'Failure'};
+    var jsonBro = JSON.stringify(rejected)
+    mongoose.connection.close()
+    _callback(jsonBro);
+  }
+
+  if(voteType == "Up"){
+  message.voteNum += 1;
+  }
+  else if(voteType == "Down"){
+    message.voteNum -= 1;
+  }
+
+  message.save(function(err,model) {
+  if (err) { 
+    var rejected = {'status':'Failure'};
+    var jsonBro = JSON.stringify(rejected)
+    mongoose.connection.close()
+    _callback(jsonBro);
+  }
+  else{
+        var success = {'status':'Success'}
+        //console.log(users.userName, users.passWord, users.emailAddress)
+        var jsonBro = JSON.stringify(model)
+        console.log(jsonBro);
+        mongoose.connection.close();
+        _callback(jsonBro);
+  }
+  });
+  });
+
+};
+
+module.exports.voting = voting;
